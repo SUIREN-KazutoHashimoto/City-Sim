84dbdb3a098862daef46120aa274ea37681c9324
Kazuto Hashimoto <kazuto.hashimoto@suiren.jp>
2026-08-25 11:30:30 +0900

    v0.1.41 sync documentation to current code

--- src/version.ts
+++ src/version.ts
@@ -2,4 +2,4 @@ import './rendering/RenderFilterTuning';
 import './rendering/RenderFilterRailSplit';
 import './rendering/UiNoiseReduction';
 
-export const APP_VERSION = '0.1.40';
+export const APP_VERSION = '0.1.41';
